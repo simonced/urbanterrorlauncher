1c9b95bc3e900c69db60feb0a93577783d07d71f
simonced <simonced@037e586a-f398-5f4b-0624-0d0cdeee5319>
2010-09-13 08:20:58 +0000

    Updated to be able to scan the Master Server. In preparation of the searching for buddies on other servers functions.

--- UrbanTerror_server_query.py
+++ UrbanTerror_server_query.py
@@ -4,7 +4,7 @@
 """
 Simonced Urban Terror Launcher
 simonced@gmail.com
-v0.2
+v0.3
 This tools is a tool to request the q3 like servers about their status online (map, players...)
 """
 
@@ -15,13 +15,15 @@ class Utsq:
 	
 	
 	#Constructeur
-	def __init__(self, host, port):
+	def __init__(self, host, port, autoGetAll_=True):
 		
 		self.data = ""	#brute data received from the server
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	#connection udp		
 		self.status = {}	#empty dict that will contain the server status response
-		self.clients = []
-		self.connected = False
+		self.clients = []       #raw data of players connected on the server
+                
+		self.raw_response = ""  #Raw response from the server after sending a query
+                self.connected = False
 		self.request = False	#allow client to know if the send request passed or failed
 		self.host = host
 		self.port = port
@@ -31,17 +33,17 @@ class Utsq:
 			self.sock.connect( (host, port) )
 			self.connected = True
 			self.sock.settimeout(1.0)  # timeout of 1 second
+
+                        #server queried automatically by default
+			if autoGetAll_:
+                            self.getServerStatus()
 			
-			#patch to get the ping value
-			start = time.time()
-			self.getall()
-			self.ping = time.time() - start
 			
 		except socket.error:
 			print "Error at connexio with %s:%s\n" % (host, port)
 
 	
-	#==
+	#===
 	#sending many kind of messages
 	def send(self, cmd_):
 		data = False
@@ -51,18 +53,26 @@ class Utsq:
 
 		try:
 			self.sock.send("\xFF\xFF\xFF\xFF"+cmd_+"\n")
-			content = self.sock.recv(65565)
-			data = content.strip().split("\n")	# part0 : headers, part1 : status, part2 players
+			#patch to get the ping value
+			start = time.time()
+                        #receiving data
+                        content = self.sock.recv(65565)
+                        self.ping = time.time() - start
+
+                        #we keep track of the total answer
+                        self.raw_response = content.strip()
+                        #we split headers and data
+                        data = self.raw_response.split("\n")	# part0 : headers, part1 : status, part2 players
+                        
 		except:
-			print self.host + "SENDING REQUEST STATUS TO SERVER FAILED"
-		
+			print self.host + "SENDING %s COMMAND TO SERVER FAILED" % cmd_
 		
 		return data
 	
 
-	#==
+	#===
 	#This function checks everything
-	def getall(self):
+	def getServerStatus(self):
 		
 		data = self.send("getstatus")
 		
@@ -85,11 +95,49 @@ class Utsq:
 		
 		self.request = True
 		return True
-	
+
+
+        #===
+        # Query the Master Server
+        def getMasterStatus(self):
+
+            #to get all servers, we can use ; getservers 68 full empty
+            #but we dont need empty servers
+            self.send("getservers 68 full")
+            data = self.raw_response
+            answer_expected = "\xFF\xFF\xFF\xFFgetserversResponse\\"
+
+            #TODO ; analysing the received data
+            if answer_expected not in data:
+                print "ERROR, WRONG ANSWER RECEIVED"
+                return False
+
+            #we work on a copy
+            data = data.replace(answer_expected, "")
+            #split by group of 7 characters
+            list = [ data[index*7:(index*7)+7] for index in range(len(data)/7) ]
+
+            servers = []
+            for address in list:
+                #print address
+                parts = [ str(ord(charac)) for charac in address ]
+                #print parts
+                address_str = ".".join( parts[0:4] )
+                address_str += ":" + str(256*ord(address[4]) + ord(address[5]))
+                #print address_str
+                servers.append(address_str)
+
+            #we finished
+            return servers
+
 	
 	#===
 	#debug script to check data receives
 	def debug(self):
+
+                print "== Raw Status =="
+                print self.raw_response
+
 		print "== Server Status =="
 		for k in self.status:
 			print k + " = " + self.status[k]
@@ -109,11 +157,20 @@ class Utsq:
 #=========
 if __name__=="__main__":
 
-	#connection	
-	test = Utsq("64.156.192.169", 27960)
-
-	
-	##simple debug
-	test.debug()
-	
-	test.close()
+	""" single server test 
+        #connection
+	test1 = Utsq("64.156.192.169", 27960)
+        #simple debug
+	test1.debug()
+        test1.close()
+        """
+        
+        """ master server query test """
+        print "=== MASTER QUERY ==="
+        #connection
+	test2 = Utsq("master.urbanterror.net", 27950, False)
+        servers = test2.getMasterStatus()
+        print servers
+        #simple debug
+	#test2.debug()
+	test2.close()
